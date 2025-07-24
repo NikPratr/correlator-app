40d7683b37c88c3ec8ddb01cab3b9c87ce31ed42
NikPratr <nikpratr@gmail.com>
2025-07-23 20:21:44 -0400

    Added multi-period functionality for calendar for tracking of various datapoints over multiple tracking periods.

--- correlator-app/src/screens/CalendarScreen.tsx
+++ correlator-app/src/screens/CalendarScreen.tsx
@@ -1,17 +1,58 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
 import { Calendar } from 'react-native-calendars';
+import { Datapoint } from '../models/datapoint';
+import { dummyDatapoints } from '../data/dummyData';
+
+/**
+ * Converts an array of Datapoint objects into a format suitable for periods in the calendar.
+ * Every date in each Datapoint's tracking periods are converted into the MarkedDate format.
+ */
+const datapointsToMarkedDates = (datapoints: Datapoint[]) => {
+    const markedDates: MarkedDates = { };
+
+    for (const datapoint of datapoints) {
+        for (const period of datapoint.history.trackingPeriods) {
+            const start = period.start;
+            // If no end, treat as ongoing until today
+            const end = period.end ? period.end : new Date();
+            const current = new Date(start);
+
+            while (current <= end) {
+                // Convert to YYYY-MM-DD
+                const dateStr = current.toISOString().split("T")[0];
+
+                if (!markedDates[dateStr]) {
+                    markedDates[dateStr] = { periods: [] };
+                }
+
+                markedDates[dateStr].periods.push({
+                    startingDay: current.getTime() === start.getTime(),
+                    endingDay: current.getTime() === end.getTime(),
+                    color: datapoint.color,
+                });
+
+                // Move to next day
+                current.setDate(current.getDate() + 1);
+            }
+        }
+    }
+
+    return markedDates;
+}
 
 const CalendarScreen = () => {
     const [selectedDate, setSelectedDate] = useState('');
-
+    
     return (
         <View style={{ flex: 1, padding: 50 }}>
             <Calendar
                 style={{ borderWidth: 1, borderColor: '#ddd', borderRadius: 10 }}
                 onDayPress={(date) => setSelectedDate(date.dateString)}
+                markingType='multi-period'
                 markedDates={{
                     [selectedDate]: { selected: true, selectedColor: '#00adf5' },
+                    ...datapointsToMarkedDates(dummyDatapoints)
                 }}
             />
 
